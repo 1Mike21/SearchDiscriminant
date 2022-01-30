3b133f7c0199e1439509e2c309df5511a6ce1ddb
Скосырев Михаил <mihail.skosyrev@yandex.ru>
2022-01-30 20:09:36 +0300

    Файлы проекта

---
+++ Descriminant/Discriminant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscriminantLib
+{
+    public class Discriminant
+    {
+        public static string CalculatingDiscriminant(double A, double B, double C)
+        {
+            double x;
+            double D = Math.Pow(B, 2) - 4 * A * C; //формула дискриминанта
+            D = Math.Sqrt(D); 
+            double x1 = (-B + D) / 2 * A;//первый корень дискриминанта
+            double x2 = (-B - D) / 2 * A;//второй корень дискриминанта
+
+            if (D > 0)
+            {
+                return $"Уравнение имеет 2 вещественных корня: x1={x1}, x2={x2}";
+                
+            }
+
+           else if (D == 0)
+            {
+                return $"Уравнение имеет 1 вещественный корень: x1={x1}";
+            }
+
+            else
+            {
+                return $"Вещественных корней нет";
+            }
+        }
+    }
+}
---
+++ SearchDiscriminant/Program.cs
@@ -0,0 +1,24 @@
+﻿using DiscriminantLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SearchDiscriminant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Введите числа А,В,С:");
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            double C = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Discriminant.CalculatingDiscriminant( A, B, C));
+            Console.ReadKey();
+        }
+    }
+}
